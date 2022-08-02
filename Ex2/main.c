45544fa0d483db3fcd12e81ab7b657d72e9e1bb3
nhat117 <s3878174@rmit.edu.vn>
2022-08-02 15:51:19 +0700

    feat(1): Finishing implent ex2

--- Ex2/main.c
+++ Ex2/main.c
@@ -2,6 +2,9 @@
 #include "NUC100Series.h"
 #define HXT_STATUS 1<<0
 #define TIMER3_COUNTS 20
+
+int pressCount = 0;
+
 int main(void)
 {
     //System initialization start-------------------
@@ -73,84 +76,58 @@ int main(void)
 
     SYS_LockReg();  // Lock protected registers
 		
+		// debounce
+		PB->DBEN |= (1 << 15);
+		GPIO->DBNCECON &= ~(1 << 4);
+		GPIO->DBNCECON &= ~(0xF << 0);
+		GPIO->DBNCECON |= 0xC << 0;
+		
 		PC->PMD &= (~(0x03<<24));
 		PC->PMD |= (0x01 <<24);
 		PC->PMD &= (~(0x03<<26));
 		PC->PMD |= (0x01 <<26);
 		
+		// Interupt config
+		PB->PMD &= (~(0x03<<30));
+		PB->IMD &= (~(1<<15));
+		PB->IEN |= (1 << 31);
+		
+		NVIC->ISER[0] |= 1 << 3;
+		NVIC->IP[0] &= ~(3 << 30);
+		
+		PB->PMD &= (~(0x03<<22)); // buzzer
+		PB->PMD |= (0x11<<22);
+		
     while (1) {
-         if (TIMER0->TISR & (1 << 0)) //Wait for the Overflow flag (TIF) to be 
-         {
-            PC->DOUT ^= (1 << 13); //Toggle
-            TIMER0->TISR |= (1 << 0); //Clear the flag by writing 1 to it
-         }
-				 
-				 if (TIMER1->TISR & (1 << 0)) //Wait for the Overflow flag (TIF) to be 
-         {
-            PC->DOUT ^= (1 << 12); //Toggle
-            TIMER1->TISR |= (1 << 0); //Clear the flag by writing 1 to it
-         }
-				
-
+					if (pressCount >= 3 && pressCount < 8) { // press 3 times
+						if (TIMER0->TISR & (1 << 0)) //Wait for the Overflow flag (TIF) to be 
+						{
+							PC->DOUT ^= (1 << 12); //Toggle
+							TIMER0->TISR |= (1 << 0); //Clear the flag by writing 1 to it
+						}
+					} 
+					else if (pressCount >= 8 && pressCount < 16) { // press 5 times
+						if (TIMER0->TISR & (1 << 0)) //Wait for the Overflow flag (TIF) to be 
+						{
+							PC->DOUT ^= (1 << 12); //Toggle
+							TIMER0->TISR |= (1 << 0); //Clear the flag by writing 1 to it
+						}
+						if (TIMER1->TISR & (1 << 0)) //Wait for the Overflow flag (TIF) to be 
+						{
+							PB->DOUT ^= (1 << 11); //Toggle
+							TIMER1->TISR |= (1 << 0); //Clear the flag by writing 1 to it
+						}
+					} 
+					else if (pressCount == 16) { // press 8 times
+						pressCount = 0;
+						PC->DOUT |= (1 << 12);
+						PB->DOUT |= (1 << 11);
+					}
     }
 }
-// //------------------------------------------- main.c CODE ENDS 
 
-//------------------------------------------- main.c CODE STARTS -------------
-// #include <stdio.h>
-// #include "NUC100Series.h"
-// #define HXT_STATUS 1<<0
-// #define TIMER0_COUNTS 800000-1
-// int main(void)
-// {
-//     //System initialization start-------------------
-//     SYS_UnlockReg(); //Unlock protected regs
-//     CLK->PWRCON |= (1 << 0);
-//         while(!(CLK->CLKSTATUS & HXT_STATUS));
-//     //Select CPU clock
-//     //12 MHz HXT
-//     CLK->CLKSEL0 &= (~(0b111<<0));
-//     CLK->PWRCON &= ~(1<<7);// Normal mode
-//     //Clock frequency divider
-//     CLK->CLKDIV &= (~(0xF<<0)); 
-//     //System initialization end---------------------
-//     //GPIO initialization start --------------------
-//     //GPIOC.14: output push-pull
-//         PC->PMD &= (~(0b11<< 28));
-//         PC->PMD |= (0b01 << 28);    
-//     //GPIO initialization end ----------------------
-//     //Timer 0 initialization start--------------
-//     //TM0 Clock selection and configuration
-//     CLK->CLKSEL1 &= ~(0b111 << 8);
-//     CLK->CLKSEL1 |= (0b010 << 8);
-//     CLK->APBCLK |= (1 << 2);
-//     //Pre-scale =11
-//     TIMER0->TCSR &= ~(0xFF << 0);
-//     TIMER0->TCSR |= 11 << 0;
-//     //reset Timer 0
-//     TIMER0->TCSR |= (1 << 26);
-//     //define Timer 0 operation mode
-//     TIMER0->TCSR &= ~(0b11 << 27);
-//     TIMER0->TCSR |= (0b01 << 27);
-//     TIMER0->TCSR &= ~(1 << 24);
-//     //TDR to be updated continuously while timer counter is counting
-//     TIMER0->TCSR |= (1 << 16);
-//     //Enable TE bit (bit 29) of TCSR
-//     //The bit will enable the timer interrupt flag TIF
-//     TIMER0->TCSR |= (1 << 29);
-//     //TimeOut = 0.8s --> Counter's TCMPR = 800000-1
-//     TIMER0->TCMPR = TIMER0_COUNTS;
-//     //start counting
-//     TIMER0->TCSR |= (1 << 30);
-//     //Timer 0 initialization end----------------
-//     SYS_LockReg();  // Lock protected registers
-//     while (1) {
-//         if (TIMER0->TISR & (1 << 0)) //Wait for the Overflow flag (TIF) to be 
+void EINT1_IRQHandler(void) {
+	++pressCount;
+	PB->ISRC |= 1 << 15;
+}
 
-//     {
-//         PC->DOUT ^= 1 << 14; //Toggle
-//         TIMER0->TISR |= (1 << 0); //Clear the flag by writing 1 to it
-//     }
-//     }
-// }
-// //------------------------------------------- main.c CODE ENDS
